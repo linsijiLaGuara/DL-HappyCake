f2484916932d26c7d96790e1a2559eff37839bf0
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-02-16 10:50:49 -0300

    add: diseño inicial}

--- happycake/src/components/Navbar.jsx
+++ happycake/src/components/Navbar.jsx
@@ -1,18 +1,44 @@
-import { Container, Navbar } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faAddressBook,
+  faShop,
+} from "@fortawesome/free-solid-svg-icons";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
+
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container className="justify-content-start">
-        <Navbar.Brand>Dogstel</Navbar.Brand>
-        <Link to="/" className="text-white ms-3 text-decoration-none">
-          Home
-        </Link>
-        <Link to="/planes" className="text-white ms-3 text-decoration-none">
-          Planes
-        </Link>
+    <Navbar className="bg-red" data-bs-theme="dark">
+      <Container>
+        <Nav>
+          <Link
+            className="text-white ms-3 text-decoration-none star-line"
+            to="#home"
+          >
+            <FontAwesomeIcon icon={faHome} className="mr-2 icon" />
+            <h6 className="m-0 align-self-center">Home</h6>
+          </Link>
+          <Link
+            className="text-white ms-3 text-decoration-none star-line"
+            to="#contacto"
+          >
+            <FontAwesomeIcon icon={faAddressBook} className="mr-2 icon" />
+            <h6 className="m-0 align-self-center">Contacto</h6>
+          </Link>
+        </Nav>
+        <Navbar.Brand to="#home" className="endLogo">
+          <h3>
+            <FontAwesomeIcon icon={faShop} className="mr-2 iconLog" />
+          </h3>
+          <h3>Happy Cake </h3>
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
 };
+
 export default NavBar;
--- happycake/src/main.jsx
+++ happycake/src/main.jsx
@@ -4,10 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
